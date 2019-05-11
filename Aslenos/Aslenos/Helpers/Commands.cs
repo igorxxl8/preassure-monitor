d238fa809c309f1f42b2ca659c48cc073dd40b13
Warsel <walot04@gmail.com>
2019-05-11 12:12:02 +0300

    Commands added

---
+++ Aslenos/Aslenos/Helpers/Commands.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aslenos.Helpers
+{
+    public static class Commands
+    {
+        public static readonly string START = "start_adc";
+        public static readonly string STOP = "stop_adc";
+    }
+}
