5d8d84a174305a02afb1449b2473b6f6d5f00970
Warsel <walot04@gmail.com>
2019-05-11 11:58:40 +0300

    Guids and Constants added

---
+++ Aslenos/Aslenos/Helpers/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aslenos.Helpers
+{
+    public static class Constants
+    {
+        public static readonly int SCAN_TIMEOUT = 10000;
+    }
+}
---
+++ Aslenos/Aslenos/Helpers/Guids.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Aslenos.Helpers
+{
+    public static class Guids
+    {
+        public static readonly Guid UART_SERVICE = Guid.Parse("6e400001-b5a3-f393-e0a9-e50e24dcca9e");
+        public static readonly Guid RX_DATA_CHARACTERISTIC = Guid.Parse("6e400003-b5a3-f393-e0a9-e50e24dcca9e");
+        public static readonly Guid TX_DATA_CHARACTERISTIC = Guid.Parse("6e400002-b5a3-f393-e0a9-e50e24dcca9e");
+    }
+}
