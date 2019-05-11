da75850c56d615e8b655bcfc8ee0a106909e7bfd
igorxxl8 <Igor Turtsevich>
2019-05-11 15:39:57 +0300

    Options page added

--- Aslenos/Aslenos/Views/OptionsPage.xaml.cs
+++ Aslenos/Aslenos/Views/OptionsPage.xaml.cs
@@ -9,12 +9,25 @@ using Xamarin.Forms.Xaml;
 
 namespace Aslenos.Views
 {
+    class Option
+    {
+        public string Name { get; set; }
+    }
+
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class OptionsPage : ContentPage
     {
         public OptionsPage()
         {
             InitializeComponent();
+            OptionsList.ItemsSource = new List<Option>
+            {
+                new Option {Name = "Pulsation analyze"},
+                new Option {Name = "Manometr"},
+                new Option {Name = "Open trend"},
+                new Option {Name = "Open trend graphic"},
+                new Option {Name = "Open vacuum"}
+            };
         }
     }
 }
