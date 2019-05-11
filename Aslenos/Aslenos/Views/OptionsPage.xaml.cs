1eefa963656af04f0ef22f0331a5dd37a5d17921
igorxxl8 <Igor Turtsevich>
2019-05-11 16:15:39 +0300

    Model Option added

--- Aslenos/Aslenos/Views/OptionsPage.xaml.cs
+++ Aslenos/Aslenos/Views/OptionsPage.xaml.cs
@@ -3,17 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Aslenos.Models;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace Aslenos.Views
 {
-    class Option
-    {
-        public string Name { get; set; }
-    }
-
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class OptionsPage : ContentPage
     {
@@ -22,12 +17,39 @@ namespace Aslenos.Views
             InitializeComponent();
             OptionsList.ItemsSource = new List<Option>
             {
-                new Option {Name = "Pulsation analyze"},
-                new Option {Name = "Manometr"},
-                new Option {Name = "Open trend"},
-                new Option {Name = "Open trend graphic"},
-                new Option {Name = "Open vacuum"}
+                new Option
+                {
+                    Name = "Pulsation analyze",
+                    Command = new Command(() => DisplayAlert("1", "", "OK"))
+                },
+                new Option
+                {
+                    Name = "Manometer",
+                    Command = new Command(() => DisplayAlert("2", "", "OK"))
+                },
+                new Option
+                {
+                    Name = "Open trend",
+                    Command = new Command(() => DisplayAlert("3", "", "OK"))
+                },
+                new Option
+                {
+                    Name = "Open trend graphic",
+                    Command = new Command(() => DisplayAlert("4", "", "OK"))
+                },
+                new Option
+                {
+                    Name = "Open vacuum",
+                    Command = new Command(() => DisplayAlert("5", "", "OK"))
+                },
             };
+
+            OptionsList.ItemSelected += OnOptionSelected; ;
+        }
+
+        private void OnOptionSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            ((Option)OptionsList.SelectedItem).Command.Execute(null);
         }
     }
 }
