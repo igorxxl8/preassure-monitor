c4ad0956b992bc759ce220026f35f46258057d7c
igorxxl8 <Igor Turtsevich>
2019-05-15 04:15:00 +0300

    In open trend graphics data panel filling added

--- Aslenos/Aslenos/Views/OpenTrendGraphicPage.xaml.cs
+++ Aslenos/Aslenos/Views/OpenTrendGraphicPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Aslenos.Models;
 using Aslenos.ViewModel;
 using Xamarin.Forms;
@@ -13,9 +14,16 @@ namespace Aslenos.Views
 
         public OpenTrendGraphicPage(DeviceDataViewModel deviceData)
         {
+            RealTimeDeviceData Comp(RealTimeDeviceData i1, RealTimeDeviceData i2) => i1.Fluctuation > i2.Fluctuation ? i1 : i2;
+
             InitializeComponent();
 
-            BindingContext = deviceData ?? new DeviceDataViewModel();
+            var data = deviceData ?? new DeviceDataViewModel();
+            BindingContext = data;
+
+
+            Ch1Data.BindingContext = data.FirstChanelSeriesData.Aggregate(Comp);
+            Ch2Data.BindingContext = data.SecondChanelSeriesData.Aggregate(Comp);
         }
 
         private void DataButton_OnClicked(object sender, EventArgs e)
